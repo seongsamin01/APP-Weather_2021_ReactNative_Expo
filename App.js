c00a5c09029272a2b3d1beb292fabe515e883f30
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-06 15:56:41 +0900

    expo-location, API, axios /재설치,코딩성공
    #Expo Location API#
    
    1) expo install expo-location (or =npm install expo-location)
    
    2) user permission 기능 추가해야됨
    
    3) npm add axios
    
    3번 마무리 과정에서 에러떠서 새로하나 다시 만들었는데, 그러니까 이제 됨.

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from 'expo-location';
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "99919bb4c713e197195d5c6ace9d21df";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+  };
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("사용자 위치를 찾을 수 없습니다", "😓");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
