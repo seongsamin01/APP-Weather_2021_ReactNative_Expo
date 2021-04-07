45dac813733028be5223f55bbf6d9fe27bc0827a
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-07 12:20:36 +0900

    각 날씨배경 Linear Gradient #2
     각 날씨에 맞는 icon과 gradient color를 넣어줬음
    =======================
    중간에 사용자 위치를 찾을 수 없다는 에러가 떴는데,  catch부분에 this.setState({ isLoading: false});를 삭제해주니 됐다.

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default class extends React.Component {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("사용자 위치를 찾을 수 없습니다", "😓");
     }
--- Weather.js
+++ Weather.js
@@ -1,26 +1,75 @@
  import React from "react";
- import { View, Text, StyleSheet } from "react-native";
+ import { View, Text, StyleSheet, StatusBar } from "react-native";
  import PropTypes from "prop-types";
  import { LinearGradient } from 'expo-linear-gradient';
  import { Ionicons } from '@expo/vector-icons';
 
- export default function Weather({ temp }) {
+ const weatherOptions = {
+     Haze : {
+        iconName: "filter",
+        gradient: ["#e65c00", "#F9D423"]
+     },
+     Thunderstorm : {
+         iconName: "ios-thunderstorm",
+         gradient: ["#000000","#434343"]
+     },
+     Drizzle : {
+         iconName: "rainy-sharp",
+         gradient: ["#636fa4","#e8cbc0"]
+     },
+     Rain : {
+         iconName: "ios-umbrella",
+         gradient: ["#021B79","#0575E6"]
+     },
+     Snow : {
+         iconName: "ios-snow-sharp",
+         gradient: ["#CFDEF3","#E0EAFC"]
+     },
+     Atmosphere : {
+         iconName: "partly-sunny-sharp",
+         gradient: ["#be93c5","#7bc6cc"]
+     },
+     Clear : {
+         iconName: "sunny",
+         gradient: ["#1c92d2","#f2fcfe"]
+     },
+     Clouds : {
+         iconName: "ios-cloudy",
+         gradient: ["#00416a","#e4e5e6"]
+     },
+     Mist : {
+         iconName: "ios-water",
+         gradient: ["#2980b9", "#6dd5fa", "#ffffff"]
+     },
+     Dust : {
+         iconName: "alert-circle",
+         gradient: ["#1d4350","#a43931"]
+     }
+ };
+
+ export default function Weather({ temp, condition }) {
     return (
-        <View style={styles.container}>
-          <LinearGradient colors={['rgba(0,0,0,0.8)', 'transparent']} style={styles.background}>
-            <View style={styles.halfContainer}>
-                <Ionicons name="ios-rainy-outline" size={150} color="blue" />
-                <Text style={styles.temp}>{temp}°C</Text>
-            </View>
-            <View style={styles.halfContainer} />
-          </LinearGradient>
-        </View> 
+        <LinearGradient 
+            colors={weatherOptions["Mist"].gradient}
+            style={styles.container}
+        >
+           <StatusBar barStyle="light-content" />
+           <View style={styles.halfContainer}>
+              <Ionicons 
+                size={150}
+                name={weatherOptions[condition].iconName}
+                color="white"
+            />
+              <Text style={styles.temp}>{temp}°C</Text>
+           </View>
+           <View style={styles.halfContainer} />
+        </LinearGradient>
           
     );      
  }
 
  Weather.propTypes = {
-    temp:PropTypes.number.isRequired,
+    temp: PropTypes.number.isRequired,
     condition: PropTypes.oneOf([
         "Thunderstorm",
         "Drizzle",
@@ -31,8 +80,7 @@
         "Clouds",
         "Haze",
         "Mist",
-        "Dust",
-        "Fog"
+        "Dust"
     ]).isRequired
  };
 
@@ -44,7 +92,8 @@
      },
      temp: {
          fontSize:28,
-         textAlign: "center"
+         textAlign: "center",
+         color: "white"
      },
      halfContainer: {
          flex: 1,
